b04600489e7c40afa7afa44063683cd11063f4f2
ELKINANY <153640440+ELKINANY@users.noreply.github.com>
2025-10-05 18:37:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const DB = require("./config/DB");
 const PORT = process.env.PORT;
 const errorMiddleware = require("./middlewares/error.middleware");
 const apiError = require("./utils/apiError");
+const cors = require('cors');
 
 DB();
 
 app.use(express.json());
+
+app.use(cors('*'));
 
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/products", require("./routes/product.route"));
